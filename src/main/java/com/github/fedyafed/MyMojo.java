af85c7be30db360db96a168c3d670133f0b6c60e
fedyafed <fedyafed@mail.ru>
2018-01-03 21:24:53 +0300

    #7 Add checkstyle plugin

--- src/main/java/com/github/fedyafed/MyMojo.java
+++ src/main/java/com/github/fedyafed/MyMojo.java
@@ -18,48 +18,39 @@ import java.io.IOException;
  * Goal which touches a timestamp file.
  */
 @Mojo(name = "touch", defaultPhase = LifecyclePhase.INITIALIZE)
-public class MyMojo
-    extends AbstractMojo
-{
+public class MyMojo extends AbstractMojo {
     /**
      * Location of the file.
      */
     @Parameter(defaultValue = "${project.build.directory}", required = true)
     private File outputDirectory;
 
+
+    /**
+     * Mojo execution.
+      */
     public void execute()
-        throws MojoExecutionException
-    {
+            throws MojoExecutionException {
         File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
+        if (!f.exists()) {
             f.mkdirs();
         }
 
-        File touch = new File( f, "touch.txt" );
+        File touch = new File(f, "touch.txt");
 
         FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
+        try {
+            w = new FileWriter(touch);
 
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
                     w.close();
-                }
-                catch ( IOException e )
-                {
+                } catch (IOException e) {
                     // ignore
                 }
             }
---
+++ src/test/java/com/github/fedyafed/MyMojoTest.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 fedyafed.
+ */
+
+package com.github.fedyafed;
+
+import junit.framework.TestCase;
+
+public class MyMojoTest extends TestCase {
+
+}

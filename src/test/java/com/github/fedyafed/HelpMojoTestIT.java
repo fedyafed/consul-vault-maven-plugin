261b117b816f8ac9cdbdcb6f808f8d6ba62861d3
fedyafed <fedyafed@mail.ru>
2019-01-27 18:49:49 +0300

    #35 Add documentation

--- src/test/java/com/github/fedyafed/HelpMojoTestIT.java
+++ src/test/java/com/github/fedyafed/HelpMojoTestIT.java
@@ -8,6 +8,8 @@ import com.github.fedyafed.generated.HelpMojo;
 import junit.framework.TestCase;
 import org.apache.maven.plugin.MojoExecutionException;
 
+import java.lang.reflect.Field;
+
 public class HelpMojoTestIT extends TestCase {
 
     /**
@@ -16,6 +18,22 @@ public class HelpMojoTestIT extends TestCase {
      */
     public void testHelp() throws MojoExecutionException {
         HelpMojo helpMojo = new HelpMojo();
+        helpMojo.execute();
+    }
+
+    /**
+     * Test generated HelpMojo with detailed report.
+     * @throws MojoExecutionException - default Mojo exception.
+     */
+    public void testDetailedHelp() throws MojoExecutionException {
+        HelpMojo helpMojo = new HelpMojo();
+        try {
+            Field detail = HelpMojo.class.getDeclaredField("detail");
+            detail.setAccessible(true);
+            detail.setBoolean(helpMojo, true);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new MojoExecutionException("Can not turn on detailed output", e);
+        }
         helpMojo.execute();
     }
 }
